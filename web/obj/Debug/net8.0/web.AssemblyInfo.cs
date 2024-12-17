60cb592d53576a894a3c4b674106001189bc4c56
Gasper Novak <gaspernovak22@gmail.com>
2024-12-17 10:33:39 +0100

    DBinitializer

--- web/obj/Debug/net8.0/web.AssemblyInfo.cs
+++ web/obj/Debug/net8.0/web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5018da7e30ea80dd393f4ac1c7131d9751229cbe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4553cbf2aa955645849fafae3936774a5e778c61")]
 [assembly: System.Reflection.AssemblyProductAttribute("web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
