130a59f7da86c46fc8b5167da75b8608a9481b39
David <david.reseta@gmail.com>
2024-12-22 12:48:25 +0100

    home screen ammounts

--- web/Controllers/HomeController.cs
+++ web/Controllers/HomeController.cs
@@ -1,20 +1,28 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using web.Models;
+using web.Data;
 
 namespace web.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly BlagajnaContext _context;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(BlagajnaContext context, ILogger<HomeController> logger)
     {
+        _context = context;
         _logger = logger;
     }
 
     public IActionResult Index()
     {
+        decimal totalAmount = _context.Transactions.Sum(t => t.Amount);
+        decimal balance = 1800 - totalAmount;
+        ViewData["balance"] = balance;
+        ViewData["TotalAmount"] = totalAmount;
+
         return View();
     }
 
--- web/obj/Debug/net8.0/web.AssemblyInfo.cs
+++ web/obj/Debug/net8.0/web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e98d8ab1e4154ef85bd2e20c875c4e0b2109207")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e75cb3ad7381d526159e616ddc050c64de98cf76")]
 [assembly: System.Reflection.AssemblyProductAttribute("web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
