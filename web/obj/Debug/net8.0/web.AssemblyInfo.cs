c1c7c1f88f9ffdc43ec114025d937ed7aba77479
David <david.reseta@gmail.com>
2024-12-27 19:09:24 +0100

    updated BudgetController

--- web/obj/Debug/net8.0/web.AssemblyInfo.cs
+++ web/obj/Debug/net8.0/web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c4a236710048ae8496d0b1a4ed4b44c0b24c277")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9abda0e8cfaa16de5640e3d290db6963f8476c04")]
 [assembly: System.Reflection.AssemblyProductAttribute("web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
