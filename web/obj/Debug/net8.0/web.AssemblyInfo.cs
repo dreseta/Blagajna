f09e727bb6f59543a76f3a0b101666e011664c4e
David <david.reseta@gmail.com>
2025-01-07 08:59:53 +0100

    app database from azure

--- web/obj/Debug/net8.0/web.AssemblyInfo.cs
+++ web/obj/Debug/net8.0/web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fc065d55371b8460da2e74bf7752fb4e02798f8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41f7aa1ddfafab6559515198436334ecc02f8346")]
 [assembly: System.Reflection.AssemblyProductAttribute("web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
